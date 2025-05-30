4ad8e8ad29db7a3b4b80f96ba094175be9961460
Ali Mohammed <engineer.ali.mohammed@fcai.usc.edu.eg>
2025-05-30 07:26:44 +0300

    Add is_admin column to users table to future tasks

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,15 +12,12 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'is_admin',
     ];
 
     /**
@@ -43,6 +40,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
         ];
     }
 }
