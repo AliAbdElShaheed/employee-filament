bd827e56f1a61ed3fcc3acf867514e3c94095f9f
Ali Mohammed <engineer.ali.mohammed@fcai.usc.edu.eg>
2025-06-03 09:01:58 +0300

    Customize create notification for employee

--- app/Filament/Resources/EmployeeResource/Pages/CreateEmployee.php
+++ app/Filament/Resources/EmployeeResource/Pages/CreateEmployee.php
@@ -3,10 +3,36 @@
 namespace App\Filament\Resources\EmployeeResource\Pages;
 
 use App\Filament\Resources\EmployeeResource;
-use Filament\Actions;
+use Filament\Notifications\Notification;
+use Filament\Notifications\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateEmployee extends CreateRecord
 {
     protected static string $resource = EmployeeResource::class;
+
+
+    /*protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Employee created successfully';
+    }*/
+
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title($this->getCreatedNotificationTitle())
+            ->success()
+            ->seconds(5)
+            ->body('The employee has been created successfully.')
+            ->actions([
+                Action::make('View')
+                    ->url($this->getResource()::getUrl('view', ['record' => $this->getRecord()]))
+                    ->color('success')
+                    ->icon('heroicon-o-eye'),
+                Action::make('undo')
+                    ->color('gray')
+                    ->icon('heroicon-o-arrow-uturn-down'),
+            ]);
+    }
 }
